e3dd52488781c853ca6fb4d1a8c03a0d6a64a5e8
Bazil Zainal <bazilzainal@gmail.com>
2022-02-09 13:59:02 +0800

    Add finish and crash logic, restart scene

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
             Debug.Log("Finished!");
+            SceneManager.LoadScene(0);
         }
     }
 }
